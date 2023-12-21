0a5beb1557f370e839516414c87338b48a6c97ae
lauradefaria <lauradfma@gmail.com>
2023-12-20 21:13:19 -0300

    Criação Classes Funcionário, Diretor, Bibliotecário e Atendente

---
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Atendente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaTech.ProjetoFinal.BibliotecaCentral
+{
+    internal class Atendente
+    {
+        //CONSTRUTOR HERDADO 
+        private void AprovarEmprestimo(ReservaLivro reservaLivro)
+        {
+
+        }
+        private void AprovarEmprestimo(ComunidadeAcademica comunidadeAcademica, Livro livro)
+        {
+
+        }
+        private Emprestimo VerificarEmprestimo(Reserva reserva, ComunidadeAcademica comunidadeAcademica, Livro livro)
+        {
+
+        }
+        private void ConfirmarDevolucao(Emprestimo emprestimo)
+        {
+
+        }
+        private void AtualizarComunidadeAcademica()
+        {
+
+        }
+        private void CadastrarComunidadeAcademica()
+        {
+
+        }
+    }
+}
