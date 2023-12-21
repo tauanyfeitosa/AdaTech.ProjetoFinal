c12d0bf77841366a023ad0f1924946ac113cb692
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2023-12-21 17:40:48 -0300

    feat: alterações nas solicitações de livros

---
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Models/Business/ISolicitacao.cs
@@ -0,0 +1,10 @@
+﻿
+namespace AdaTech.ProjetoFinal.BibliotecaCentral.Models.Business
+{
+    internal interface ISolicitacao
+    {
+        void AprovarSolicitacao(Diretor diretor);
+        void AlterarSolicitacao(Livro livro, TipoAcervo tipoAcervo, string descricao);
+        void ReprovarSolicitacao(Diretor diretor);
+    }
+}
---
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Models/Business/SolicitacaoRequisicaoLivros.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaTech.ProjetoFinal.BibliotecaCentral.Models.Business
+{
+    internal class SolicitacaoRequisicaoLivros
+    {
+        private readonly Bibliotecario _bibliotecario;
+        //private Livro _livro;
+        private bool _aprovada;
+
+        internal Bibliotecario Bibliotecario { get => _bibliotecario;}
+        internal string Descricao { get => _descricao; set => _descricao = value; }
+        internal bool Aprovada { get => _aprovada; set => _aprovada = value; }
+
+        public void AprovarSolicitacao(Diretor diretor)
+        {
+            if (diretor == null)
+                throw new ArgumentNullException(nameof(diretor));
+            this.Aprovada = true;
+        }
+
+        public void AlterarSolicitacao(Livro livro = null, TipoAcervo tipoAcervo = null, string descricao = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReprovarSolicitacao(Diretor diretor)
+        {
+            if (diretor == null)
+                throw new ArgumentNullException(nameof(diretor));
+            this.Aprovada = false;
+        }
+    }
+}
