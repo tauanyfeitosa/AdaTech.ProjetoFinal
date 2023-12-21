355a135ef8d84f2066fc1207c6c74adcf2dcb44f
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2023-12-21 18:47:33 -0300

    fix: reenvio das classes, erro no pull request original

---
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Models/Business/Livro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaTech.ProjetoFinal.BibliotecaCentral
+{
+    internal class Livro
+    {
+        private string _titulo { get; set; }
+        private string _autor { get; set; }
+        private string _isbn { get; set; }
+        private int _anoPublicacao { get; set; }
+        private int _edicao { get; set; }
+        private string _editora { get; set; }
+        private int _exemplares { get; set; }
+        private int _exemplaresDisponiveis { get; set; }
+        private int _livrosBomEstado { get; set; }
+        private int _livrosEstadoMediano { get; set; }
+        private int _livrosMauEstado { get; set; }
+        private TipoAcervoLivro _tipoAcervoLivro { get; set; }
+
+    }
+}
