7753b2239636255088fdb7f5fba4d479d3b7c3e1
AmandaaBastos <amandabastoscorreia@gmail.com>
2023-12-23 00:41:58 -0300

    Classes ReservaLivro, ReservaLivroData e o Enum StatusReserva
    Implementado o esqueleto das classes

---
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Models/Business/Reserva/ReservaLivro.cs
@@ -0,0 +1,63 @@
+﻿using AdaTech.ProjetoFinal.BibliotecaCentral;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdaTech.ProjetoFinal.BibliotecaCentral.Models.Business.Reserva
+{
+    internal class ReservaLivro
+    {
+        private int _numeroReserva;
+        private Livro _livro;
+        private ComunidadeAcademica _usuarioComunidadeAcademica;
+        private DateTime _dataRetirarLivro;
+        private DateTime _dataReserva;
+        private StatusReserva _statusReserva;
+
+        internal int NumeroReserva { get { return _numeroReserva; } }
+        internal Livro Livro { get { return _livro; } }
+        internal ComunidadeAcademica UsuarioComunidadeAcademica { get { return _usuarioComunidadeAcademica; } }
+        internal DateTime DataRetirarLivro { get { return _dataRetirarLivro; } }
+        internal DateTime DataReserva { get { return _dataReserva; } }
+        internal StatusReserva StatusReserva
+        {
+            get { return _statusReserva; }
+            set { _statusReserva = value; }
+        }
+
+        internal ReservaLivro(int numeroReserva, Livro livro, ComunidadeAcademica usuarioComunidadeAcademica,
+            DateTime dataRetirarLivro, DateTime dataReserva, StatusReserva statusReserva)
+        {
+            this._numeroReserva = numeroReserva;
+            this._livro = livro;
+            this._usuarioComunidadeAcademica = usuarioComunidadeAcademica;
+            this._dataReserva = dataReserva;
+            this._dataRetirarLivro = dataRetirarLivro;
+            this._statusReserva = statusReserva;
+        }
+
+        //internal void AtualizarDataRetirada()
+        //{
+
+        //}
+
+        //internal void AprovarReserva()
+        //{
+
+        //}
+
+        //internal void CancelarReserva()
+        //{
+
+        //}
+
+        //internal void CancelarReservaUsuario()
+        //{
+
+        //}
+    }
+}
+
---
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Models/Business/Reserva/ReservaLivroData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaTech.ProjetoFinal.BibliotecaCentral.Models.Business.Reserva
+{
+    internal static class ReservaLivroData
+    {
+        private static Tuple<List<ReservaLivro>, List<ReservaLivro>> _reservaLivros;
+
+        //internal static void AtualizarJsonResrvas()
+        //{
+            
+        //}
+        //internal static List<ReservaLivro> ListarReservasUsuario(Usuario usuario)
+        //{
+
+
+        //}
+        //internal static void SelecionarReserva(ReservaLivro numeroReserva)
+        //{
+
+        //}
+        //internal static void ExcluirReservas(ReservaLivro numeroReserva)
+        //{
+
+        //}
+    }
+}
