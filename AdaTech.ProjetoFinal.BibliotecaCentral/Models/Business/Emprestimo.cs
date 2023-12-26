f5d085c109da0736688b5a74ef99426dab0ed8a5
lauradefaria <lauradfma@gmail.com>
2023-12-26 01:04:20 -0300

    Criação classes Emprestimo, EmprestimoData e Multa

---
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Models/Business/Emprestimo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaTech.ProjetoFinal.BibliotecaCentral.Models.Business
+{
+    internal class Emprestimo
+    {
+        private ReservaLivro _reservaLivro;
+        private Livro _livro;
+        private ComunidadeAcademica _usuarioComunidadeAcademica;
+        private DateTime _dataEmprestimo;
+        private DateTime _dataDevolucaoPrevista;
+        private DateTime _dataDevolucaoUsuario;
+        private bool _devolucao;
+        private Multa _multaAtraso;
+
+        internal ReservaLivro ReservaLivro { get { return _reservaLivro; } }
+        internal Livro Livro { get { return _livro; } }
+        internal ComunidadeAcademica ComunidadeAcademica { get { return _usuarioComunidadeAcademica; } }
+        internal Multa Multa { get { return _multaAtraso; } }
+
+        internal DateTime DataEmprestimo
+        {
+            get
+            {
+                return _dataEmprestimo;
+            }
+            private set
+            {
+                _dataEmprestimo = value;
+            }
+        }
+        internal DateTime DataDevolucaoPrevista
+        {
+            get
+            {
+                return _dataDevolucaoPrevista;
+            }
+            private set
+            {
+                _dataDevolucaoPrevista = value;
+            }
+        }
+        internal DateTime DataDevolucaoUsuario
+        {
+            get
+            {
+                return _dataDevolucaoUsuario;
+            }
+            private set
+            {
+                _dataDevolucaoUsuario = value;
+            }
+        }
+        internal bool Devolucao
+        {
+            get
+            {
+                return _devolucao;
+            }
+            private set
+            {
+                _devolucao = value;
+            }
+        }
+
+        internal void DevolverLivro(string isbn)
+        {
+
+        }
+        internal void RenovarLivro()
+        {
+
+        }
+    }
+}
---
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Models/Business/EmprestimoData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaTech.ProjetoFinal.BibliotecaCentral.Models.Business
+{
+    using Usuarios.UsuariosComunidadeAcademica;
+    internal class EmprestimoData
+    {
+        private static List<Emprestimo> _emprestimoLivros = new List<Emprestimo>();
+        internal List<Emprestimo> SelecionarEmprestimo(ComunidadeAcademica usuario)
+        {
+
+        }
+        internal List<Emprestimo> SelecionarEmprestimo(Emprestimo emprestimos)
+        {
+
+        }
+
+    }
+}
