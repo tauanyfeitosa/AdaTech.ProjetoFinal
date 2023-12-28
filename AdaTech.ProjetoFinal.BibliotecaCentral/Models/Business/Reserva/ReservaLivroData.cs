8f079a655cd3ea8db6eec0af7782ba7cf4d4d8e1
AmandaaBastos <amandabastoscorreia@gmail.com>
2023-12-28 00:09:04 -0300

    feat(Reserva): Adição de alterações em ReservaLivroData

--- AdaTech.ProjetoFinal.BibliotecaCentral/Models/Business/Reserva/ReservaLivroData.cs
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Models/Business/Reserva/ReservaLivroData.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AdaTech.ProjetoFinal.BibliotecaCentral.Models.Usuarios.UsuariosComunidadeAcademica;
+using AdaTech.ProjetoFinal.BibliotecaCentral.Models.Business.AcervoLivros;
+using System.Windows.Forms;
 
 namespace AdaTech.ProjetoFinal.BibliotecaCentral.Models.Business.Reserva
 {
@@ -14,24 +17,88 @@ namespace AdaTech.ProjetoFinal.BibliotecaCentral.Models.Business.Reserva
         //{
 
         //}
-        internal static List<ReservaLivro> ListarReservasUsuario(Usuario usuario)
+        internal static List<ReservaLivro> ListarReservasUsuario(ComunidadeAcademica usuario)
         {
-            List<ReservaLivro> reservasUsuario = _reservasLivros.Item1.Where
-                (r => r.UsuarioComunidadeAcademica == usuario).ToList();
-            return reservasUsuario;
-        }       
-        
-        internal static void SelecionarReserva(ReservaLivro numeroReserva)
-        {
-            //esse eu não sei o que faz T.T
+            if (usuario.TipoUsuario == TipoUsuarioComunidade.Professor)
+            {
+                List<ReservaLivro> reservasUsuario = _reservasLivros.Item1.Where
+                    (r => r.UsuarioComunidadeAcademica == usuario).ToList();
+                return reservasUsuario;
+            }
+            else if (usuario.TipoUsuario == TipoUsuarioComunidade.Aluno)
+            {
+                List<ReservaLivro> reservasUsuario = _reservasLivros.Item2.Where
+                    (r => r.UsuarioComunidadeAcademica == usuario).ToList();
+                return reservasUsuario;
+            }
+            else
+            {
+                return null;
+            }
         }
-        internal static void ExcluirReservas(ReservaLivro numeroReserva)
+        internal static ReservaLivro SelecionarReserva(ReservaLivro reserva, ComunidadeAcademica usuario)
         {
-           _reservasLivros.Item1.Remove(numeroReserva);
+            if (usuario.TipoUsuario == TipoUsuarioComunidade.Professor)
+            {
+                List<ReservaLivro> reservasUsuario = _reservasLivros.Item1.Where
+                    (r => r.UsuarioComunidadeAcademica == usuario).ToList();
+                ReservaLivro reservaSelecionada = reservasUsuario
+                    .FirstOrDefault(r =>r.NumeroReserva == reserva.NumeroReserva);
+                return reservaSelecionada;
+            }
+            else if (usuario.TipoUsuario == TipoUsuarioComunidade.Aluno)
+            {
+                List<ReservaLivro> reservasUsuario = _reservasLivros.Item2.Where
+                    (r => r.UsuarioComunidadeAcademica == usuario).ToList();
+                ReservaLivro reservaSelecionada = reservasUsuario
+                    .FirstOrDefault(r => r.NumeroReserva == reserva.NumeroReserva);
+                return reservaSelecionada;               
+            }
+            else
+            {
+                throw new InvalidOperationException("Não foi possível encontrar a reserva.");
+            }
         }
-        internal static void AdicionarReserva(ReservaLivro reserva)
+        internal static void ExcluirReservas(ReservaLivro numeroReserva, ComunidadeAcademica usuario)
         {
-            _reservasLivros.Item1.Add(reserva);
+            if (usuario.TipoUsuario == TipoUsuarioComunidade.Professor)
+            {
+                List<ReservaLivro> reservasUsuario = _reservasLivros.Item1.Where
+                    (r => r.UsuarioComunidadeAcademica == usuario).ToList();
+                ReservaLivro reservaSelecionada = reservasUsuario
+                    .FirstOrDefault(r => r.NumeroReserva == numeroReserva.NumeroReserva);
+                reservasUsuario.Remove(reservaSelecionada);
+            }
+            else if (usuario.TipoUsuario == TipoUsuarioComunidade.Aluno)
+            {
+                List<ReservaLivro> reservasUsuario = _reservasLivros.Item2.Where
+                    (r => r.UsuarioComunidadeAcademica == usuario).ToList();
+                ReservaLivro reservaSelecionada = reservasUsuario
+                    .FirstOrDefault(r => r.NumeroReserva == numeroReserva.NumeroReserva);
+                reservasUsuario.Remove(reservaSelecionada);
+            }
+            else
+            {
+                throw new InvalidOperationException("Não foi possível excluir a reserva.");
+            }   
         }
+        internal static void AdicionarReserva(Livro livro, ComunidadeAcademica usuarioComunidadeAcademica,
+            DateTime dataRetirarLivro, DateTime dataReserva)
+        {
+            if (usuarioComunidadeAcademica.TipoUsuario == TipoUsuarioComunidade.Professor)
+            {
+                var numeroReserva = _reservasLivros.Item1.Count;
+                _reservasLivros.Item1.Add(new ReservaLivro(numeroReserva, livro, usuarioComunidadeAcademica, dataRetirarLivro, dataReserva));
+            }
+            else if (usuarioComunidadeAcademica.TipoUsuario == TipoUsuarioComunidade.Aluno)
+            {
+                var numeroReserva = _reservasLivros.Item2.Count;
+                _reservasLivros.Item1.Add(new ReservaLivro(numeroReserva, livro, usuarioComunidadeAcademica, dataRetirarLivro, dataReserva));
+            }
+            else
+            {
+                throw new InvalidOperationException("Não foi possível adicionar a reserva.");
+            }
+        } 
     }
 }
