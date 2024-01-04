26ae951ac5bb58d49dd87e6c867aa17402f3f86c
lauradefaria <lauradfma@gmail.com>
2024-01-04 02:12:48 -0300

    Mudança do atributo "Ativo" de Funcionário para Usuário

--- AdaTech.ProjetoFinal.BibliotecaCentral/Models/Usuarios/Usuario.cs
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Models/Usuarios/Usuario.cs
@@ -9,7 +9,7 @@ namespace AdaTech.ProjetoFinal.BibliotecaCentral
    internal abstract class Usuario
     {
         protected string login, senhaCripto, nomeCompleto, cpf, email;
-        private bool _ehAdmin;
+        private bool _ehAdmin, _ativo;
 
         internal string Login
         { 
@@ -27,6 +27,11 @@ namespace AdaTech.ProjetoFinal.BibliotecaCentral
         { 
             get { return _ehAdmin;  } 
             set{ if(VerificaEhAdmin()) _ehAdmin = value; }
+        }
+        internal bool Ativo
+        {
+            get { return _ativo; }
+            set { _ativo = value; }
         }
 
         protected Usuario(string senha, string nomeCompleto, string cpf, string email)
