15d373bb62ec4e29479eae4671abdc311adfe409
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2024-01-05 18:56:51 -0300

    feat(janelas/controllers): criação das janelas e lógicas de botões
    foram criados os controllers de apoio para cada view correspondente

---
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Controllers/PrincipalControllers/PainelAtendenteController/VisualizarProfessoresController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using AdaTech.ProjetoFinal.BibliotecaCentral.Models.Usuarios.UsuariosData;
+using AdaTech.ProjetoFinal.BibliotecaCentral.Views.Janelas.JanelasAtendente;
+
+namespace AdaTech.ProjetoFinal.BibliotecaCentral.Controllers
+{
+    public class VisualizarProfessoresController
+    {
+        private JanelaVisualizarProfessores form;
+
+        public VisualizarProfessoresController(JanelaVisualizarProfessores form)
+        {
+            this.form = form;
+            form.Load += (sender, e) => ExibirProfessores();
+        }
+
+        public void ExibirProfessores()
+        {
+            List<ComunidadeAcademica> professores = UsuarioData.ObterProfessores();
+
+            foreach (ComunidadeAcademica professor in professores)
+            {
+                form.AdicionarProfessorNaListBox($"{professor.NomeCompleto} - {professor.Matricula} - {professor.Curso}");
+            }
+        }
+    }
+}
---
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Views/Janelas/JanelasDiretor/JanelaVisualizarFuncionarios.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AdaTech.ProjetoFinal.BibliotecaCentral.Models.Usuarios.UsuariosData;
+
+namespace AdaTech.ProjetoFinal.BibliotecaCentral.Views.Janelas.JanelasDiretor
+{
+    public partial class JanelaVisualizarFuncionarios : Form
+    {
+        private ListBox listBoxFuncionarios = new ListBox();
+        public JanelaVisualizarFuncionarios()
+        {
+            InitializeComponent();
+            ConfigurarListBox();
+            ExibirFuncionarios();
+        }
+
+        private void ConfigurarListBox()
+        {
+            listBoxFuncionarios.Dock = DockStyle.Fill;
+            listBoxFuncionarios.ScrollAlwaysVisible = true;
+            listBoxFuncionarios.SelectionMode = SelectionMode.None;
+
+            Controls.Add(listBoxFuncionarios);
+
+            listBoxFuncionarios.DrawMode = DrawMode.OwnerDrawVariable;
+            listBoxFuncionarios.MeasureItem += ListBoxFuncionarios_MeasureItem;
+            listBoxFuncionarios.DrawItem += ListBoxFuncionarios_DrawItem;
+        }
+
+        private void ListBoxFuncionarios_MeasureItem(object sender, MeasureItemEventArgs e)
+        {
+            e.ItemHeight = 30;
+        }
+
+        private void ListBoxFuncionarios_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index < 0)
+                return;
+
+            e.Graphics.DrawString(listBoxFuncionarios.Items[e.Index].ToString(), e.Font, Brushes.Black, e.Bounds.Left, e.Bounds.Top + 5);
+
+            e.Graphics.DrawLine(Pens.Gray, e.Bounds.Left, e.Bounds.Bottom - 1, e.Bounds.Right, e.Bounds.Bottom - 1);
+        }
+
+        private void ExibirFuncionarios()
+        {
+            List<Funcionario> funcionarios = UsuarioData.ObterFuncionarios();
+
+            foreach (Funcionario funcionario in funcionarios)
+            {
+                listBoxFuncionarios.Items.Add($"{funcionario.NomeCompleto} - {funcionario.Cpf} - {funcionario.Cargo}");
+            }
+        }
+    }
+}
