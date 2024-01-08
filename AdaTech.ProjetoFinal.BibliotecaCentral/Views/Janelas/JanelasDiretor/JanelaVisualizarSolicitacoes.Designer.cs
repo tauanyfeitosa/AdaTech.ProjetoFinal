516f1dda2cc5e5a074628bf783bb11480aaae61a
Tauany Feitosa <tauanysanttos13@gmail.com>
2024-01-08 17:35:17 -0300

    fix (VisualizarSolicitacoes)
    mudança na estrutura do formulário para abstrações necessárias

--- AdaTech.ProjetoFinal.BibliotecaCentral/Views/Janelas/JanelasDiretor/JanelaVisualizarSolicitacoes.Designer.cs
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Views/Janelas/JanelasDiretor/JanelaVisualizarSolicitacoes.Designer.cs
@@ -1,16 +1,14 @@
-﻿namespace AdaTech.ProjetoFinal.BibliotecaCentral.Views.Janelas.JanelasDiretor
+﻿using AdaTech.ProjetoFinal.BibliotecaCentral.Models.Business.Solicitacoes;
+using System.Windows.Forms;
+
+namespace AdaTech.ProjetoFinal.BibliotecaCentral.Views.Janelas.JanelasDiretor
 {
     partial class JanelaVisualizarSolicitacoes
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+        private DataGridView dgvSolicitacoes;
+        private Usuario usuario;
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,16 +20,20 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "JanelaVisualizarSolicitacoes";
+
+            dgvSolicitacoes = new DataGridView();
+            dgvSolicitacoes.Dock = DockStyle.Top;
+            dgvSolicitacoes.AutoGenerateColumns = true;
+            dgvSolicitacoes.ReadOnly = true;
+            dgvSolicitacoes.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            Controls.Add(dgvSolicitacoes);
         }
 
         #endregion
--- AdaTech.ProjetoFinal.BibliotecaCentral/Views/Janelas/JanelasDiretor/JanelaVisualizarSolicitacoes.cs
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Views/Janelas/JanelasDiretor/JanelaVisualizarSolicitacoes.cs
@@ -7,49 +7,31 @@ namespace AdaTech.ProjetoFinal.BibliotecaCentral.Views.Janelas.JanelasDiretor
 {
     internal partial class JanelaVisualizarSolicitacoes : Form
     {
-        private DataGridView dgvMudancaAcervo;
-        private DataGridView dgvRequisicaoLivros;
-        private Usuario usuario;
-
         public JanelaVisualizarSolicitacoes(Usuario usuario, TipoSolicitacao tipoSolicitacao)
         {
             this.usuario = usuario;
+            InitializeComponent();
             if (usuario is Diretor diretor)
-                InitializeDataGridViews(tipoSolicitacao);
+            {
+                if (tipoSolicitacao is TipoSolicitacao.MudarAcervoLivro)
+                {
+                    dgvSolicitacoes.DataSource = SolicitacoesData.SelecionarMudancaAcervo();
+                }
+                else
+                {
+                    dgvSolicitacoes.DataSource = SolicitacoesData.SelecionarRequisicaoLivros();
+                }
+
+                dgvSolicitacoes.Height = this.ClientSize.Height / 2;
+                dgvSolicitacoes.CellDoubleClick += (sender, e) => MostrarDetalhes((ISolicitacao)dgvSolicitacoes.SelectedRows[0].DataBoundItem);
+            }
+            
             this.WindowState = FormWindowState.Maximized;
         }
 
         private void InitializeDataGridViews(TipoSolicitacao solicitacao)
         {
-            Controls.Clear();
-            if (solicitacao == TipoSolicitacao.MudarAcervoLivro)
-            {
-                dgvMudancaAcervo = new DataGridView();
-                dgvMudancaAcervo.Dock = DockStyle.Top;
-                dgvMudancaAcervo.Height = this.ClientSize.Height / 2;
-                dgvMudancaAcervo.AutoGenerateColumns = true;
-                dgvMudancaAcervo.ReadOnly = true;
-                dgvMudancaAcervo.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-                dgvMudancaAcervo.DataSource = SolicitacoesData.SelecionarMudancaAcervo();
-
-                dgvMudancaAcervo.CellDoubleClick += (sender, e) => MostrarDetalhes((ISolicitacao)dgvMudancaAcervo.SelectedRows[0].DataBoundItem);
-
-                Controls.Add(dgvMudancaAcervo);
-            }
-            else
-            {
-                dgvRequisicaoLivros = new DataGridView();
-                dgvRequisicaoLivros.Dock = DockStyle.Top;
-                dgvRequisicaoLivros.Height = this.ClientSize.Height / 2;
-                dgvRequisicaoLivros.AutoGenerateColumns = true;
-                dgvRequisicaoLivros.ReadOnly = true;
-                dgvRequisicaoLivros.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-                dgvRequisicaoLivros.DataSource = SolicitacoesData.SelecionarRequisicaoLivros();
-
-                dgvRequisicaoLivros.CellDoubleClick += (sender, e) => MostrarDetalhes((ISolicitacao)dgvRequisicaoLivros.SelectedRows[0].DataBoundItem);
-
-                Controls.Add(dgvRequisicaoLivros);
-            }
+            
 
         }
 
