06a5a33bd1452d204c6c2ab5d1ed5c090a245611
Tauany Feitosa <tauanysanttos13@gmail.com>
2024-01-08 17:22:05 -0300

    fix(DetalhesSolicitacao)
    separação dos formulários

--- AdaTech.ProjetoFinal.BibliotecaCentral/Views/Janelas/JanelasDiretor/JanelaDetalhesSolicitacao.cs
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Views/Janelas/JanelasDiretor/JanelaDetalhesSolicitacao.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Windows.Forms;
-using AdaTech.ProjetoFinal.BibliotecaCentral.Controllers;
 using AdaTech.ProjetoFinal.BibliotecaCentral.Models.Business.Solicitacoes;
 
 namespace AdaTech.ProjetoFinal.BibliotecaCentral.Views.Janelas.JanelasDiretor
@@ -13,69 +12,24 @@ namespace AdaTech.ProjetoFinal.BibliotecaCentral.Views.Janelas.JanelasDiretor
         public JanelaDetalhesSolicitacao(ISolicitacao solicitacao)
         {
             this.solicitacao = solicitacao;
-            InitializeComponents();
-        }
+            InitializeComponent();
 
-        private void InitializeComponents()
-        {
-            Text = "Detalhes da Solicitação";
-            Size = new System.Drawing.Size(400, 300);
-
-            Label lblBibliotecario = new Label();
-            lblBibliotecario.Dock = DockStyle.Top;
             lblBibliotecario.Text = $"Bibliotecário: {GetBibliotecarioNome()}";
-
-            Label lblDescricao = new Label();
-            lblDescricao.Dock = DockStyle.Top;
             lblDescricao.Text = $"Descrição: {GetDescricao()}";
-
-            Label lblTipoAcervo = new Label();
-            lblTipoAcervo.Dock = DockStyle.Top;
             lblTipoAcervo.Text = $"Tipo de Acervo: {GetTipoAcervo()}";
-
-            Label lblLivroTitulo = new Label();
-            lblLivroTitulo.Dock = DockStyle.Top;
             lblLivroTitulo.Text = $"Título do Livro: {GetLivroTitulo()}";
-
-            Label lblLivroIsbn = new Label();
-            lblLivroIsbn.Dock = DockStyle.Top;
             lblLivroIsbn.Text = $"ISBN do Livro: {GetLivroIsbn()}";
-
-            Label lblAprovada = new Label();
-            lblAprovada.Dock = DockStyle.Top;
             lblAprovada.Text = $"Aprovada: {GetAprovada()}";
 
-            Button btnAprovar = new Button();
-            btnAprovar.Dock = DockStyle.Bottom;
-            btnAprovar.Text = "Aprovar";
             btnAprovar.Click += (sender, e) =>
             {
                 AprovarSolicitacao();
             };
-
-            Button btnReprovar = new Button();
-            btnReprovar.Dock = DockStyle.Bottom;
-            btnReprovar.Text = "Reprovar";
             btnReprovar.Click += (sender, e) =>
             {
                 ReprovarSolicitacao();
             };
-
-            Button btnFechar = new Button();
-            btnFechar.Dock = DockStyle.Bottom;
-            btnFechar.Text = "Fechar";
             btnFechar.Click += (sender, e) => Close();
-
-            Controls.Add(lblBibliotecario);
-            Controls.Add(lblDescricao);
-            Controls.Add(lblTipoAcervo);
-            Controls.Add(lblLivroTitulo);
-            Controls.Add(lblLivroIsbn);
-            Controls.Add(lblAprovada);
-
-            Controls.Add(btnAprovar);
-            Controls.Add(btnReprovar);
-            Controls.Add(btnFechar);
         }
 
         private string GetBibliotecarioNome()
