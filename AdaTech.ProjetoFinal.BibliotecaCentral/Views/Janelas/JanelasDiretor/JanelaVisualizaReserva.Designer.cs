f32101cb56b994a5c58055e03449557defb1c2f7
Tauany Feitosa <tauanysanttos13@gmail.com>
2024-01-08 17:25:19 -0300

    fix (VisualizarReserva)
    separação do formulário

--- AdaTech.ProjetoFinal.BibliotecaCentral/Views/Janelas/JanelasDiretor/JanelaVisualizaReserva.Designer.cs
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Views/Janelas/JanelasDiretor/JanelaVisualizaReserva.Designer.cs
@@ -1,16 +1,12 @@
-﻿namespace AdaTech.ProjetoFinal.BibliotecaCentral.Views.Janelas.JanelasDiretor
+﻿using System.Windows.Forms;
+
+namespace AdaTech.ProjetoFinal.BibliotecaCentral.Views.Janelas.JanelasDiretor
 {
     partial class JanelaVisualizaReserva
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+        private ListBox listBoxReserva = new ListBox();
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,12 +18,22 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Janela Visualizar Reservas";
+
+            listBoxReserva.Dock = DockStyle.Fill;
+            listBoxReserva.ScrollAlwaysVisible = true;
+            listBoxReserva.SelectionMode = SelectionMode.None;
+
+            Controls.Add(listBoxReserva);
+
+            listBoxReserva.DrawMode = DrawMode.OwnerDrawVariable;
+            listBoxReserva.MeasureItem += ListBoxReservas_MeasureItem;
+            listBoxReserva.DrawItem += ListBoxReservas_DrawItem;
         }
 
         #endregion
