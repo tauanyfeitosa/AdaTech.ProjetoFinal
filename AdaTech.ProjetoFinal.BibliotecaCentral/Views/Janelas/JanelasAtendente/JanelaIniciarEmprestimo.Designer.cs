656f2c095f04f2f2cf1494a4afa189f3b9986531
Tauany Feitosa <tauanysanttos13@gmail.com>
2024-01-08 16:17:52 -0300

    fix (IniciarEmprestimo)
    separação do forms para alteração automatizada do design

--- AdaTech.ProjetoFinal.BibliotecaCentral/Views/Janelas/JanelasAtendente/JanelaIniciarEmprestimo.Designer.cs
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Views/Janelas/JanelasAtendente/JanelaIniciarEmprestimo.Designer.cs
@@ -1,16 +1,17 @@
-﻿namespace AdaTech.ProjetoFinal.BibliotecaCentral.Views.Janelas.JanelasAtendente
+﻿using System.Windows.Forms;
+using System;
+
+namespace AdaTech.ProjetoFinal.BibliotecaCentral.Views.Janelas.JanelasAtendente
 {
     partial class JanelaIniciarEmprestimo
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        public event EventHandler<CellClickEventArgs> CellClick;
+        public event EventHandler CriarSemReservaButtonClick;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        private DataGridView tabela;
+        private Button btnCriarSemReserva;
+        private System.ComponentModel.IContainer components = null;
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,18 +23,113 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabela = new System.Windows.Forms.DataGridView();
+            this.colunaID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colunaLivro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colunaAutor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colunaUsuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colunaTipoUsuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnCriarSemReserva = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tabela)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabela
+            // 
+            this.tabela.AllowUserToAddRows = false;
+            this.tabela.AllowUserToDeleteRows = false;
+            this.tabela.ColumnHeadersHeight = 34;
+            this.tabela.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colunaID,
+            this.colunaLivro,
+            this.colunaAutor,
+            this.colunaUsuario,
+            this.colunaTipoUsuario});
+            this.tabela.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabela.Location = new System.Drawing.Point(0, 0);
+            this.tabela.Name = "tabela";
+            this.tabela.ReadOnly = true;
+            this.tabela.RowHeadersWidth = 62;
+            this.tabela.Size = new System.Drawing.Size(800, 450);
+            this.tabela.TabIndex = 1;
+            // 
+            // colunaID
+            // 
+            this.colunaID.DataPropertyName = "NumeroReserva";
+            this.colunaID.HeaderText = "ID";
+            this.colunaID.MinimumWidth = 8;
+            this.colunaID.Name = "colunaID";
+            this.colunaID.ReadOnly = true;
+            this.colunaID.Width = 150;
+            // 
+            // colunaLivro
+            // 
+            this.colunaLivro.DataPropertyName = "LivroTitulo";
+            this.colunaLivro.HeaderText = "Livro";
+            this.colunaLivro.MinimumWidth = 8;
+            this.colunaLivro.Name = "colunaLivro";
+            this.colunaLivro.ReadOnly = true;
+            this.colunaLivro.Width = 150;
+            // 
+            // colunaAutor
+            // 
+            this.colunaAutor.DataPropertyName = "LivroAutor";
+            this.colunaAutor.HeaderText = "Autor";
+            this.colunaAutor.MinimumWidth = 8;
+            this.colunaAutor.Name = "colunaAutor";
+            this.colunaAutor.ReadOnly = true;
+            this.colunaAutor.Width = 150;
+            // 
+            // colunaUsuario
+            // 
+            this.colunaUsuario.DataPropertyName = "UsuarioNome";
+            this.colunaUsuario.HeaderText = "Usuario";
+            this.colunaUsuario.MinimumWidth = 8;
+            this.colunaUsuario.Name = "colunaUsuario";
+            this.colunaUsuario.ReadOnly = true;
+            this.colunaUsuario.Width = 150;
+            // 
+            // colunaTipoUsuario
+            // 
+            this.colunaTipoUsuario.DataPropertyName = "UsuarioTipo";
+            this.colunaTipoUsuario.HeaderText = "Tipo de Usuário";
+            this.colunaTipoUsuario.MinimumWidth = 8;
+            this.colunaTipoUsuario.Name = "colunaTipoUsuario";
+            this.colunaTipoUsuario.ReadOnly = true;
+            this.colunaTipoUsuario.Width = 150;
+            // 
+            // btnCriarSemReserva
+            // 
+            this.btnCriarSemReserva.AutoSize = true;
+            this.btnCriarSemReserva.Font = new System.Drawing.Font("Arial", 12F);
+            this.btnCriarSemReserva.Location = new System.Drawing.Point(0, 413);
+            this.btnCriarSemReserva.Name = "btnCriarSemReserva";
+            this.btnCriarSemReserva.Size = new System.Drawing.Size(358, 37);
+            this.btnCriarSemReserva.TabIndex = 0;
+            this.btnCriarSemReserva.Text = "Criar Empréstimo Sem Reserva";
+            // 
+            // JanelaIniciarEmprestimo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCriarSemReserva);
+            this.Controls.Add(this.tabela);
+            this.Name = "JanelaIniciarEmprestimo";
             this.Text = "JanelaIniciarEmprestimo";
+            ((System.ComponentModel.ISupportInitialize)(this.tabela)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private DataGridViewTextBoxColumn colunaID;
+        private DataGridViewTextBoxColumn colunaLivro;
+        private DataGridViewTextBoxColumn colunaAutor;
+        private DataGridViewTextBoxColumn colunaUsuario;
+        private DataGridViewTextBoxColumn colunaTipoUsuario;
     }
 }
--- AdaTech.ProjetoFinal.BibliotecaCentral/Views/Janelas/JanelasAtendente/JanelaIniciarEmprestimo.cs
+++ AdaTech.ProjetoFinal.BibliotecaCentral/Views/Janelas/JanelasAtendente/JanelaIniciarEmprestimo.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Windows.Forms;
 using AdaTech.ProjetoFinal.BibliotecaCentral.Controllers;
 using AdaTech.ProjetoFinal.BibliotecaCentral.Models.Business.Reserva;
@@ -9,60 +8,16 @@ namespace AdaTech.ProjetoFinal.BibliotecaCentral.Views.Janelas.JanelasAtendente
 {
     internal partial class JanelaIniciarEmprestimo : Form
     {
-        public event EventHandler<CellClickEventArgs> CellClick;
-        public event EventHandler CriarSemReservaButtonClick;
-
-        private DataGridView tabela;
-        private Button btnCriarSemReserva;
 
         public JanelaIniciarEmprestimo()
         {
             InitializeComponent();
-            InicializarBotao();
-            InicializarDataGrid();
+
+            tabela.CellClick += (sender, e) => CellClick?.Invoke(sender, new CellClickEventArgs(e.RowIndex, e.ColumnIndex));
+            btnCriarSemReserva.Click += (sender, e) => CriarSemReservaButtonClick?.Invoke(sender, e);
 
             IniciarEmprestimoController controller = new IniciarEmprestimoController(this);
             controller.CarregarReservas();
-        }
-
-        private void InicializarDataGrid()
-        {
-            tabela = new DataGridView();
-
-            tabela.Dock = DockStyle.Fill;
-            tabela.AllowUserToAddRows = false;  
-            tabela.AllowUserToDeleteRows = false;  
-            tabela.ReadOnly = true;  
-
-            DataGridViewTextBoxColumn colunaID = new DataGridViewTextBoxColumn();
-            colunaID.HeaderText = "ID";
-            colunaID.DataPropertyName = "NumeroReserva";  
-            tabela.Columns.Add(colunaID);
-
-            DataGridViewTextBoxColumn colunaLivro = new DataGridViewTextBoxColumn();
-            colunaLivro.HeaderText = "Livro";
-            colunaLivro.DataPropertyName = "LivroTitulo";  
-            tabela.Columns.Add(colunaLivro);
-
-            DataGridViewTextBoxColumn colunaAutor = new DataGridViewTextBoxColumn();
-            colunaAutor.HeaderText = "Autor";
-            colunaAutor.DataPropertyName = "LivroAutor";  
-            tabela.Columns.Add(colunaAutor);
-
-            DataGridViewTextBoxColumn colunaUsuario = new DataGridViewTextBoxColumn();
-            colunaUsuario.HeaderText = "Usuario";
-            colunaUsuario.DataPropertyName = "UsuarioNome";  
-            tabela.Columns.Add(colunaUsuario);
-
-            DataGridViewTextBoxColumn colunaTipoUsuario = new DataGridViewTextBoxColumn();
-            colunaTipoUsuario.HeaderText = "Tipo de Usuário";
-            colunaTipoUsuario.DataPropertyName = "UsuarioTipo";
-            tabela.Columns.Add (colunaTipoUsuario);
-
-
-            tabela.CellClick += (sender, e) => CellClick?.Invoke(sender, new CellClickEventArgs(e.RowIndex, e.ColumnIndex));
-
-            Controls.Add(tabela);
         }
 
 
@@ -76,17 +31,6 @@ namespace AdaTech.ProjetoFinal.BibliotecaCentral.Views.Janelas.JanelasAtendente
             return rowIndex >= 0 ? tabela.Rows[rowIndex].DataBoundItem as ReservaLivro : null;
         }
 
-        private void InicializarBotao()
-        {
-            btnCriarSemReserva = new Button();
-            btnCriarSemReserva.Text = "Criar Empréstimo Sem Reserva";
-            btnCriarSemReserva.AutoSize = true;
-            btnCriarSemReserva.Font = new Font("Arial", 12, FontStyle.Regular);
-            btnCriarSemReserva.Location = new Point(10, 400);
-            btnCriarSemReserva.Click += (sender, e) => CriarSemReservaButtonClick?.Invoke(sender, e);
-
-            Controls.Add(btnCriarSemReserva);
-        }
 
         public void MostrarMensagem(string mensagem)
         {
